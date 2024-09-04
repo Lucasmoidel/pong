a8d42e8241f3ccc5d23ec5659c0f1452bbfcfd51
Lucasmoidel <99782884+Lucasmoidel@users.noreply.github.com>
2024-09-03 21:14:55 -0700

    new

--- main.c
+++ main.c
@@ -12,10 +12,12 @@ int game_running = false;
 int last_frame_time = 0;
 int p1score = 0;
 int p2score = 0;
-int yspeed = 10000;
+int yspeed = 50;
+int ballspeed = 3;
+float deltatime;
 TTF_Font* font = NULL;
 
-struct p1paddle {
+struct padle {
     float x, y;
     int width, height;
 } p1paddle;
@@ -23,6 +25,11 @@ struct p2paddle {
     float x, y;
     int width, height;
 } p2paddle;
+struct ball {
+    float x, y;
+    int w, h;
+    int xdir, ydir;
+} ball;
 
 void display_text();
 
@@ -46,6 +53,12 @@ void setup() {
     p2paddle.y = Win_height/2;
     p2paddle.width = 10;
     p2paddle.height = 75;
+    ball.x = 400;
+    ball.y = 400;
+    ball.w = 20;
+    ball.h = 20;
+    ball.xdir = 35;
+    ball.ydir = 50;
 }
 
 int main (int argc, char *argv[]) {
@@ -67,7 +80,7 @@ int main (int argc, char *argv[]) {
 }
 
 void process_input() {
-    float deltatime = (SDL_GetTicks() - last_frame_time) / 1000.0f ;
+    deltatime = (SDL_GetTicks() - last_frame_time) / 1000.0f ;
     SDL_Event event;
     SDL_PollEvent(&event);
 
@@ -82,16 +95,34 @@ void process_input() {
                     game_running = false;
                     break;
                 case SDLK_w:
-                    p1paddle.y -= yspeed * deltatime;
+                    p1paddle.y -= yspeed ;
                     break;
                 case SDLK_s:
-                    p1paddle.y += yspeed * deltatime;
+                    p1paddle.y += yspeed ;
                     break;
                 case SDLK_UP:
-                    p2paddle.y -= yspeed * deltatime;
+                    p2paddle.y -= yspeed ;
                     break;
                 case SDLK_DOWN:
-                    p2paddle.y += yspeed * deltatime;
+                    p2paddle.y += yspeed ;
+                    break;
+                case SDLK_r:
+                    p1score = 0;
+                    p2score = 0;
+                    p1paddle.x = 10;
+                    p1paddle.y = Win_height/2;
+                    p1paddle.width = 10;
+                    p1paddle.height = 75;
+                    p2paddle.x = 780;
+                    p2paddle.y = Win_height/2;
+                    p2paddle.width = 10;
+                    p2paddle.height = 75;
+                    ball.x = 400;
+                    ball.y = 400;
+                    ball.w = 20;
+                    ball.h = 20;
+                    ball.xdir = 35;
+                    ball.ydir = 50;
                     break;
                 default:
                     break;    
@@ -116,6 +147,25 @@ void update() {
         p2paddle.y = Win_height - p2paddle.height;
     }
 
+    if (ball.x <= 0) {
+        ball.xdir = ball.xdir * -1;
+        p2score += 1;
+    }
+    if (ball.x >= Win_width - ball.w) {
+        ball.xdir = ball.xdir * -1;
+        p1score += 1;
+    }
+    if (ball.y <= 0 || ball.y + ball.h >= Win_height) {
+        ball.ydir = ball.ydir * -1;
+    }
+    
+    if ((ball.x <= p1paddle.x + p1paddle.width && ball.y >= p1paddle.y && ball.y <= p1paddle.y + p1paddle.height) || (ball.x  + ball.w >= p2paddle.x && ball.y >= p2paddle.y && ball.y <= p2paddle.y + p2paddle.height)) {
+        ball.xdir = ball.xdir * -1;
+        //ball.ydir = ball.ydir * -1;
+    }
+    ball.x += ball.xdir * deltatime * ballspeed;
+    ball.y += ball.ydir * deltatime * ballspeed;
+
 }
 
 void render() {
@@ -135,7 +185,14 @@ void render() {
     (int)p2paddle.width, 
     (int)p2paddle.height 
     };
+    SDL_Rect ballrect = {
+        (float)ball.x,
+        (float)ball.y,
+        (int)ball.w,
+        (int)ball.h
+    };
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_RenderFillRect(renderer, &ballrect);
     SDL_RenderFillRect(renderer, &p1paddle_rect);
     SDL_RenderFillRect(renderer, &p2paddle_rect);
     SDL_RenderPresent(renderer);
